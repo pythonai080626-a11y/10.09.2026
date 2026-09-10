4e3ae73f83c234decba3ccc7d468c989f096afb1
fullstack290623 <fullstack290623@gmail.com>
2026-09-10 21:35:11 +0300

    upload

--- 01_ex.py
+++ 01_ex.py
@@ -41,9 +41,55 @@
 # print(pair_sum([1, 2, 3, 4, 5], 9))  # (4, 5)
 # print(pair_sum([1, 2, 3, 4, 5], 40))  # None
 # print(pair_sum([0, -2, 18, 39], 37))  # (-2, 39)
+# print(pair_sum([0, 5, 5, 39], 10))  # (5, 5)  {0, 5}
+# print(pair_sum([0, 5, 5, 39], 10))  # (5, 5)
 
 # **BONUS** : run without nested only 1 time on the numbers
 
+# [1, 3, 6], 12?
+# {1 , 3, 6}
+# [0, 5, 5, 39], 10?
+# {0, 5}
 
+# 4
+# bubble sort
+# [5, -2, 3, 0, 15, 0, 3]
+# def buble_sort(list1)
+#   return sorted list , use bubble sort
+
+# for 0 .. len
+# for 0 .. len - 1
+# for 0 .. len - 2
+# [ 5, -2, 3, 0, 15, 0, 3] 0
+# [-2, 5, 0, 3, 15, 0, 3] 1
+# [-2, 0, 5, 3, 0, 15, 3] 2
+# [-2, 0, 3, 5, 0, 15, 3] 3
+# [-2, 0, 3, 0, 5, 3, 15] 4
+# [-2, 0, 0, 3, 3, 5, 15] 5
+# [-2, 0, 0, 3, 3, 5, 15] 6 -- Done!
+
+# 5
+# insert sorted
+# def insert_sorted(list1, number)
+#    add the number while keep the list sorted
+#    if list is empty , append the number
+#    else:
+#        for i in range(len(list1)):
+#            if list1 in index i > number --> insert at index i
+#        if reach end of list
+#            append(number)
+
+
+# my_list = []
+# insert_sorted(my_list, 10)
+# print(my_list)  # [10]
+# insert_sorted(my_list, 5)
+# print(my_list)  # [5, 10]
+# insert_sorted(my_list, 8)
+# print(my_list)  # [5, 8, 10]
+# insert_sorted(my_list, 30)
+# print(my_list)  # [5, 8, 10, 30]
+# insert_sorted(my_list, -1)
+# print(my_list)  # [-1, 5, 8, 10, 30]
 
 
