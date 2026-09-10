bb3c3627ca2c4e5508892005e1c4beeaa6cb0970
fullstack290623 <fullstack290623@gmail.com>
2026-09-10 18:19:49 +0300

    Initial commit

---
+++ 01_ex.py
@@ -0,0 +1,15 @@
+
+# 1
+# password check
+# function which gets a user_dict and name, password
+
+# def check_password(user_dict, name, password)
+#     if name does not exist in dict - return 'user name {name} does not exist'
+#     if name exit but password not match - return 'wrong password'
+#     if name exist + password correct - return 'success'
+
+# test the function
+# { 'danny': 'Danny1999', 'sharon': 'Password1!' }
+# print(check_password('yossi', 'the-king')) #  --> 'user name yossi does not exist'
+# print(check_password('danny', 'the-king'))  # --> 'wrong password'
+# print(check_password('sharon', 'Password1!'))  # --> 'success'
