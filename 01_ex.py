366938f3f5ca7ac3852b219dc92447eae655941b
fullstack290623 <fullstack290623@gmail.com>
2026-09-10 19:56:29 +0300

    upload

--- 01_ex.py
+++ 01_ex.py
@@ -19,12 +19,31 @@
 # function which gets a dict and return dict
 
 # { 'a': 1, 'd': 'hi' } --> { 1: 'a', 'hi': 'd'}
-# BONUS
+# ***BONUS***
 # { 'a': 1, 'd': 1, 'c': 3 } --> { 1: ['a', 'd'], 3: 'c' }
 # { 'a': 1, 'd': 1, 'c': 3 , 'f': 1} --> { 1: ['a', 'd', 'f'], 3: 'c' }
 # def dict_swap(dict_input):
 #   return (1) dict swapped key-value
-#          (2) BONUS-- on same key return list
+#          (2) ***BONUS*** on same key return list
 #  test
 # print(dict_swap({ 'a': 1, 'd': 'hi' }))  # { 1: 'a', 'hi': 'd'}
 # print(dict_swap({ 'a': 1, 'd': 1, 'c': 3 , 'f': 1}))  # { 1: ['a', 'd', 'f'], 3: 'c' }
+
+# 3
+# find pair sum
+# function which gets a list and a sum_number
+# the function should return tuple of 2 numbers which sum of them equals sum_number
+#   if there are none --> return None
+
+# def pair_sum(list1, sum_number)
+#    return (a, b) or None
+
+# print(pair_sum([1, 2, 3, 4, 5], 9))  # (4, 5)
+# print(pair_sum([1, 2, 3, 4, 5], 40))  # None
+# print(pair_sum([0, -2, 18, 39], 37))  # (-2, 39)
+
+# **BONUS** : run without nested only 1 time on the numbers
+
+
+
+
